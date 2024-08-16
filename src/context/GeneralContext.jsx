f4e019247cd08ee10a0c42ebf8abd77443646765
mfelfelani72@gmail.com <mfcompany21@gmail.com>
2024-08-16 09:45:06 +0330

    dsfsd

---
+++ src/context/GeneralContext.jsx
@@ -0,0 +1,59 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+// import i18n from "../i18n";
+
+import axios from "../api/axios";
+
+const GeneralContext = createContext({});
+
+export const GeneralProvider = ({ children }) => {
+
+    // General
+    const navigate = useNavigate();
+    const csrf = () => axios.get('/sanctum/csrf-cookie');
+    const [user, setUser] = useState(null);
+
+    const getUser = async () => {
+
+        await csrf();
+        const { data } = await axios.get('/api/users');
+        setUser(data);
+
+    }
+
+    // const logout = () => {
+    //     axios.post("/logout").then(() => {
+    //         setUser(null);
+    //     });
+    //     navigate("/");
+    // }
+    // General
+
+    useEffect(() => {
+
+        if (!user) {
+            getUser();
+        }
+    }, []);
+
+    return <GeneralContext.Provider value={{
+
+        // General
+        user,
+        // logout,
+        getUser,
+        // General
+
+
+
+
+    }}>
+        {children}
+    </GeneralContext.Provider>
+
+}
+
+export default function useGeneralContext() {
+    return useContext(GeneralContext);
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
+import { GeneralProvider } from './context/GeneralContext.jsx';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+// import './i18n/index.js';
+
+import ReactDOM from 'react-dom/client';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <GeneralProvider>
+      
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+
+    </GeneralProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
